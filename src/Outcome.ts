aec6ce6f1b3f04d90883cb28ff9576a272a47650
Ryan Mitchener <ryanmitchener@gmail.com>
2020-01-02 10:52:59 -0500

    Converted Outcome namespace to a const object

--- src/Outcome.ts
+++ src/Outcome.ts
@@ -1,13 +1,13 @@
-export namespace Outcome {
-    export function val<T>(value: T): OutcomeValue<T> {
+export const Outcome = {
+    val: <T>(value: T): OutcomeValue<T> => {
         return new OutcomeValue(value)
-    }
+    },
 
-    export function err<T>(error: T): OutcomeError<T> {
+    err: <T>(error: T): OutcomeError<T> => {
         return new OutcomeError(error)
-    }
+    },
 
-    export async function wrap<T>(promise: Promise<T>): Promise<Outcome<T>> {
+    wrap: async <T>(promise: Promise<T>): Promise<Outcome<T>> => {
         try {
             return new OutcomeValue(await promise)
         } catch (e) {
@@ -17,7 +17,7 @@ export namespace Outcome {
 }
 
 class OutcomeError<T = unknown> {
-    constructor(public error: T) {}
+    constructor(public error: T) { }
 
     isError(): this is OutcomeError {
         return true
@@ -25,7 +25,7 @@ class OutcomeError<T = unknown> {
 }
 
 class OutcomeValue<T> {
-    constructor(public value: T) {}
+    constructor(public value: T) { }
 
     isError(): this is OutcomeError {
         return false
