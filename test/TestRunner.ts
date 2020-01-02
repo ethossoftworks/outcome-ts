bc3d13a3401b243b4230636ebe32f2afe467cc05
Ryan Mitchener <ryanmitchener@gmail.com>
2020-01-02 12:58:51 -0500

    Updated to using tabs

--- test/TestRunner.ts
+++ test/TestRunner.ts
@@ -1,63 +1,63 @@
 export type TestGroup<T> = {
-    context: T
-    tests: { [key: string]: Test<T> }
+	context: T
+	tests: { [key: string]: Test<T> }
 }
 
 export type Test<T> = (context: T) => Promise<any>
 
 export class TestRunner {
-    static async run<T>(preTestGroupSetup: ((context: T) => void) | null = null, ...testGroups: TestGroup<T>[]) {
-        let totalTestCount: number = 0
-        let totalPassedCount: number = 0
+	static async run<T>(preTestGroupSetup: ((context: T) => void) | null = null, ...testGroups: TestGroup<T>[]) {
+		let totalTestCount: number = 0
+		let totalPassedCount: number = 0
 
-        for (let i = 0; i < testGroups.length; i++) {
-            const group: TestGroup<T> = testGroups[i]
-            const singleTests: string[] = Object.getOwnPropertyNames(group.tests).filter(testName => {
-                return testName.indexOf("_") === 0
-            })
-            const testNames: string[] = singleTests.length > 0 ? singleTests : Object.getOwnPropertyNames(group.tests)
-            const context = testGroups[i].context
-            let passedCount: number = 0
-            totalTestCount += testNames.length
+		for (let i = 0; i < testGroups.length; i++) {
+			const group: TestGroup<T> = testGroups[i]
+			const singleTests: string[] = Object.getOwnPropertyNames(group.tests).filter(testName => {
+				return testName.indexOf("_") === 0
+			})
+			const testNames: string[] = singleTests.length > 0 ? singleTests : Object.getOwnPropertyNames(group.tests)
+			const context = testGroups[i].context
+			let passedCount: number = 0
+			totalTestCount += testNames.length
 
-            console.group(`Running Group (${i + 1} of ${testGroups.length})`)
-            preTestGroupSetup?.(context)
+			console.group(`Running Group (${i + 1} of ${testGroups.length})`)
+			preTestGroupSetup?.(context)
 
-            for (let j = 0; j < testNames.length; j++) {
-                const testName = testNames[j]
-                const test = group.tests[testName]
+			for (let j = 0; j < testNames.length; j++) {
+				const testName = testNames[j]
+				const test = group.tests[testName]
 
-                try {
-                    console.groupCollapsed(`Running Test (${j + 1} of ${testNames.length}): ${testName}`)
-                    await test(context)
-                    passedCount++
-                    totalPassedCount++
-                    console.groupEnd()
-                    console.log(`%cPassed:  ${testName}`, "color: green")
-                } catch (e) {
-                    console.groupEnd()
-                    console.groupCollapsed(`%cFailed:  ${testName}`, "color: red")
-                    console.log(e)
-                    console.groupEnd()
-                }
-            }
+				try {
+					console.groupCollapsed(`Running Test (${j + 1} of ${testNames.length}): ${testName}`)
+					await test(context)
+					passedCount++
+					totalPassedCount++
+					console.groupEnd()
+					console.log(`%cPassed:  ${testName}`, "color: green")
+				} catch (e) {
+					console.groupEnd()
+					console.groupCollapsed(`%cFailed:  ${testName}`, "color: red")
+					console.log(e)
+					console.groupEnd()
+				}
+			}
 
-            const color = passedCount === testNames.length ? "color: green" : "color: red"
-            console.groupEnd()
-            console.log(`%cFinished tests: ${passedCount} of ${testNames.length} passed`, color)
-        }
+			const color = passedCount === testNames.length ? "color: green" : "color: red"
+			console.groupEnd()
+			console.log(`%cFinished tests: ${passedCount} of ${testNames.length} passed`, color)
+		}
 
-        const color = totalPassedCount === totalTestCount ? "color: green" : "color: red"
-        console.log(
-            `%c\nFinished all test groups: ${totalPassedCount} of ${totalTestCount} passed`,
-            `font-weight: 700; ${color}`
-        )
-    }
+		const color = totalPassedCount === totalTestCount ? "color: green" : "color: red"
+		console.log(
+			`%c\nFinished all test groups: ${totalPassedCount} of ${totalTestCount} passed`,
+			`font-weight: 700; ${color}`
+		)
+	}
 }
 
 export function assert(condition: boolean): boolean {
-    if (!condition) {
-        throw "Test failed"
-    }
-    return condition
+	if (!condition) {
+		throw "Test failed"
+	}
+	return condition
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
 const path = require("path")
 
 const prodConfig = {
-    entry: "./src/Outcome.ts",
-    devtool: "source-map",
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    output: {
-        filename: "Outcome.js",
-        path: path.resolve(__dirname, "dist")
-    }
+	entry: "./src/Outcome.ts",
+	devtool: "source-map",
+	mode: "production",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"]
+	},
+	output: {
+		filename: "Outcome.js",
+		path: path.resolve(__dirname, "dist")
+	}
 }
 
 const testConfig = {
-    ...prodConfig,
-    ...{
-        entry: "./test/Outcome.test.ts",
-        mode: "development",
-        target: "node",
-        output: {
-            filename: "Outcome.test.js",
-            path: path.resolve(__dirname, "dist")
-        }
-    }
+	...prodConfig,
+	...{
+		entry: "./test/Outcome.test.ts",
+		mode: "development",
+		target: "node",
+		output: {
+			filename: "Outcome.test.js",
+			path: path.resolve(__dirname, "dist")
+		}
+	}
 }
 
 module.exports = env => {
-    switch (env) {
-        case "prod":
-            return prodConfig
-        case "test":
-            return testConfig
-    }
+	switch (env) {
+		case "prod":
+			return prodConfig
+		case "test":
+			return testConfig
+	}
 }
