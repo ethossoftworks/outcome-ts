04f012a5adf50bcdfa621c849fcbae2e87868a85
Ryan Mitchener <ryanmitchener@gmail.com>
2020-01-04 23:50:29 -0500

    Converted from tabs to spaces. Added setup and teardown to TestRunner

--- src/Outcome.ts
+++ src/Outcome.ts
@@ -1,35 +1,35 @@
 export const Outcome = {
-	val: <T>(value: T): OutcomeValue<T> => {
-		return new OutcomeValue(value)
-	},
+    val: <T>(value: T): OutcomeValue<T> => {
+        return new OutcomeValue(value)
+    },
 
-	err: <T>(error: T): OutcomeError<T> => {
-		return new OutcomeError(error)
-	},
+    err: <T>(error: T): OutcomeError<T> => {
+        return new OutcomeError(error)
+    },
 
-	wrap: async <T>(promise: Promise<T>): Promise<Outcome<T>> => {
-		try {
-			return new OutcomeValue(await promise)
-		} catch (e) {
-			return new OutcomeError(e)
-		}
-	}
+    wrap: async <T>(promise: Promise<T>): Promise<Outcome<T>> => {
+        try {
+            return new OutcomeValue(await promise)
+        } catch (e) {
+            return new OutcomeError(e)
+        }
+    }
 }
 
 class OutcomeError<T = unknown> {
-	constructor(public error: T) {}
+    constructor(public error: T) {}
 
-	isError(): this is OutcomeError {
-		return true
-	}
+    isError(): this is OutcomeError {
+        return true
+    }
 }
 
 class OutcomeValue<T> {
-	constructor(public value: T) {}
+    constructor(public value: T) {}
 
-	isError(): this is OutcomeError {
-		return false
-	}
+    isError(): this is OutcomeError {
+        return false
+    }
 }
 
 export type Outcome<T, E = unknown> = OutcomeValue<T> | OutcomeError<E>
--- test/TestRunner.ts
+++ test/TestRunner.ts
@@ -1,82 +1,90 @@
 export type TestGroup<T> = {
-	context: T
-	tests: { [key: string]: Test<T> }
+    context: T
+    tests: { [testName: string]: Test<T> }
+    label?: string
+    beforeEach?: (context: T) => void
+    afterEach?: (context: T) => void
+    beforeAll?: (context: T) => void
+    afterAll?: (context: T) => void
 }
 
 export type Test<T> = (context: T) => Promise<any>
 
 export class TestRunner {
-	static async run<T>(preTestGroupSetup: ((context: T) => void) | null = null, ...testGroups: TestGroup<T>[]) {
-		let totalTestCount: number = 0
-		let totalPassedCount: number = 0
+    static async run<T>(...testGroups: TestGroup<T>[]) {
+        let totalTestCount: number = 0
+        let totalPassedCount: number = 0
 
-		for (let i = 0; i < testGroups.length; i++) {
-			const group: TestGroup<T> = testGroups[i]
-			const singleTests: string[] = Object.getOwnPropertyNames(group.tests).filter(testName => {
-				return testName.indexOf("_") === 0
-			})
-			const testNames: string[] = singleTests.length > 0 ? singleTests : Object.getOwnPropertyNames(group.tests)
-			const context = testGroups[i].context
-			let passedCount: number = 0
-			totalTestCount += testNames.length
+        for (let i = 0; i < testGroups.length; i++) {
+            const group: TestGroup<T> = testGroups[i]
+            const singleTests: string[] = Object.getOwnPropertyNames(group.tests).filter(testName => {
+                return testName.indexOf("_") === 0
+            })
+            const testNames: string[] = singleTests.length > 0 ? singleTests : Object.getOwnPropertyNames(group.tests)
+            const context = group.context
+            let passedCount: number = 0
+            totalTestCount += testNames.length
 
-			console.group(`Running Group (${i + 1} of ${testGroups.length})`)
-			preTestGroupSetup?.(context)
+            console.group(`Running Group (${i + 1} of ${testGroups.length})${group.label ? ": " + group.label : ""}`)
+            group.beforeAll?.(context)
 
-			for (let j = 0; j < testNames.length; j++) {
-				const testName = testNames[j]
-				const test = group.tests[testName]
+            for (let j = 0; j < testNames.length; j++) {
+                group.beforeEach?.(context)
+                const testName = testNames[j]
+                const test = group.tests[testName]
 
-				try {
-					console.groupCollapsed(`Running Test (${j + 1} of ${testNames.length}): ${testName}`)
-					await test(context)
-					passedCount++
-					totalPassedCount++
-					console.groupEnd()
-					console.log(fmt(`Passed:  ${testName}`, Text.green))
-				} catch (e) {
-					console.groupEnd()
-					console.groupCollapsed(fmt(`Failed:  ${testName}`, Text.red))
-					console.log(e)
-					console.groupEnd()
-				}
-			}
+                try {
+                    console.groupCollapsed(`Running Test (${j + 1} of ${testNames.length}): ${testName}`)
+                    await test(context)
+                    passedCount++
+                    totalPassedCount++
+                    console.groupEnd()
+                    console.log(fmt(`\u2713 Passed:  ${testName}`, Text.green))
+                } catch (e) {
+                    console.groupEnd()
+                    console.groupCollapsed(fmt(`\u2717 Failed:  ${testName}`, Text.red))
+                    console.log(e)
+                    console.groupEnd()
+                }
+                group.afterEach?.(context)
+            }
 
-			console.groupEnd()
-			console.log(
-				fmt(
-					`Finished tests: ${passedCount} of ${testNames.length} passed`,
-					passedCount === testNames.length ? Text.green : Text.red
-				)
-			)
-		}
+            group.afterAll?.(context)
+            console.groupEnd()
+            console.log(
+                fmt(
+                    `Finished tests: ${passedCount} of ${testNames.length} passed`,
+                    passedCount === testNames.length ? Text.green : Text.red
+                )
+            )
+        }
 
-		console.log(
-			fmt(
-				`\nFinished all test groups: ${totalPassedCount} of ${totalTestCount} passed`,
-				totalPassedCount === totalTestCount ? Text.green : Text.red,
-				Text.bold,
-				Text.inverse
-			)
-		)
-	}
+        console.log(
+            fmt(
+                `\nFinished all test groups: ${totalPassedCount} of ${totalTestCount} passed`,
+                totalPassedCount === totalTestCount ? Text.green : Text.red,
+                Text.bold,
+                Text.inverse
+            )
+        )
+    }
 }
 
 export function assert(condition: boolean): boolean {
-	if (!condition) {
-		throw "Test failed"
-	}
-	return condition
+    if (!condition) {
+        throw "Test failed"
+    }
+    return condition
 }
 
 enum Text {
-	green = `\x1b[32m`,
-	red = `\x1b[31m`,
-	bold = `\x1b[1m`,
-	underline = `\x1b[4m`,
-	inverse = `\x1b[7m`
+    green = `\x1b[32m`,
+    red = `\x1b[31m`,
+    bold = `\x1b[1m`,
+    underline = `\x1b[4m`,
+    inverse = `\x1b[7m`
 }
 
 function fmt(message: string, ...formatters: Text[]): string {
-	return `${formatters.join("")}${message}\x1b[0m`
+    return `${formatters.join("")}${message}\x1b[0m`
 }
